060ea8499783f0a850ea09614cf3b44585bd863a
Roman <r.monahov2014@yandex.ru>
2020-06-30 14:16:23 +0300

    Initial commit

--- SpriteKitFirst/GameScene.swift
+++ SpriteKitFirst/GameScene.swift
@@ -10,80 +10,78 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    //MARK: - 1. Создаем экземпляр Node
+    var dog: SKSpriteNode!
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
+        //MARK: Размер задника
+        let width = UIScreen.main.bounds.width * 2
+        let height = UIScreen.main.bounds.height * 2
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let undertaleBackground = SKSpriteNode(imageNamed: "Undertale")
+        undertaleBackground.position = CGPoint(x: 667, y: 375)
+        undertaleBackground.size = CGSize(width: width, height: height)
+       
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+    //MARK: - 2. Init Node
+        dog = SKSpriteNode(imageNamed: "UndertaleDog")
+        dog.position = CGPoint(x: 100, y: 350)
+        dog.size = CGSize(width: 150, height: 150)
+
+        dog.physicsBody = SKPhysicsBody(circleOfRadius: max(dog.size.width / 2, dog.size.height / 2))
+        dog.physicsBody?.isDynamic = false
+        
+        
+        addChild(undertaleBackground)
+        addChild(dog)
+        
+        
+        //MARK: - Создание цетков
+        
+        let flowerCreate = SKAction.run {
+            let flower = self.createFlowers
+            self.addChild(flower())
         }
+        let flowerCreationDelay = SKAction.wait(forDuration: 0.5, withRange: 0.5)
+        let flowerSequenceAction = SKAction.sequence([flowerCreate, flowerCreationDelay])
+        let flowerRunAction = SKAction.repeatForever(flowerSequenceAction)
+        run(flowerRunAction)
+    
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        if let touch = touches.first{
+        //MARK: - 3. Определяем точку прикосновения
+            let touchLocation = touch.location(in: self)
+            
+        print(touchLocation)
+            
+        //MARK: - 4. Создаем действие
+            let moveAction = SKAction.move(to: touchLocation, duration: 0.6)
+            dog.run(moveAction)
         }
+    }
+    
+    
+    func createFlowers() -> SKSpriteNode {
+        let flower = SKSpriteNode(imageNamed: "BadFlower")
+        flower.xScale = 0.7
+        flower.yScale = 0.7
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        flower.position.y = CGFloat.random(in: 0 ..< 750)
+        flower.position.x = frame.size.width + flower.size.width
+
+        flower.physicsBody = SKPhysicsBody(circleOfRadius: max(flower.size.width / 2, flower.size.height / 2))
+        
+        return flower
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+       
+
     }
+    
 }
